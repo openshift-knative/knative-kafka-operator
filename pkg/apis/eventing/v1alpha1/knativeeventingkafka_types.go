6375a72c9a3500ca5566d186dcc4b515c3a0f1bd
Matthias Wessendorf <mwessend@redhat.com>
2019-06-06 17:32:59 +0200

    bootstrapServers is required

--- pkg/apis/eventing/v1alpha1/knativeeventingkafka_types.go
+++ pkg/apis/eventing/v1alpha1/knativeeventingkafka_types.go
@@ -20,7 +20,6 @@ type KnativeEventingKafkaSpec struct {
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
 
-	// +optional
 	BootstrapServers string `json:"bootstrapServers,omitempty"`
 }
 
